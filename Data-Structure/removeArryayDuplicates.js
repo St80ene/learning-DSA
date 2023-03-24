53ddad4e18a444eaca85c59ab9d1b0dda3333f21
etiene <etienejames5@gmail.com>
2023-03-24 12:37:17 +0100

    Added log to debug

--- Data-Structure/removeArryayDuplicates.js
+++ Data-Structure/removeArryayDuplicates.js
@@ -21,6 +21,7 @@
 // };
 
 function isValidPhoneNumber(phoneNumber) {
+  console.log('here');
   // Regex to check valid
   // International Phone Numbers
   const phoneNumberRegex = /^[0-9]{1,4}[-\s]?[0-9]{6,12}$/;
